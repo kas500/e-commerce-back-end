30bdd595c269aeac24f0cbb22916b4de11c8538e
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-12-24 16:41:56 -0800

    Adds get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  
   // be sure to include its associated Category and Tag data
 });
 
